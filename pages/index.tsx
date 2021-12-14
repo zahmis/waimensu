49d5b59ad056e0dd5f1dc5d75f586879bb641278
zahmis <meda829ratteru86@gmail.com>
2021-12-15 00:26:04 +0900

    add tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,48 @@ const Home = () => {
         <div className="container text-center mx-auto my-auto">
           <div className="row text-5xl mb-5">Waimensu</div>
           <hr />
+          <ul className="flex border-b">
+            <li className="-mb-px mr-1">
+              <a
+                className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="mr-1">
+              <a
+                className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+                href="#"
+              >
+                Biography
+              </a>
+            </li>
+            <li className="mr-1">
+              <a
+                className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+                href="#"
+              >
+                ワイ習慣
+              </a>
+            </li>
+            <li className="mr-1">
+              <a
+                className="bg-white inline-block py-2 px-4 text-gray-400 font-semibold"
+                href="#"
+              >
+                ワイトレ
+              </a>
+            </li>
+            <li className="mr-1">
+              <a
+                className="bg-white inline-block py-2 px-4 text-gray-400 font-semibold"
+                href="#"
+              >
+                ワイアビリティ
+              </a>
+            </li>
+          </ul>
           <History />
         </div>
         <form className="" onSubmit={handleSubmit(onSubmit)}>
