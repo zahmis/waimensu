328cbe5991b8ccf8347ce8005ccf0322126859be
zahmis <meda829ratteru86@gmail.com>
2021-12-18 21:51:11 +0900

    use enum

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const HomeContents = () => {
 const Home = () => {
   // getArticles();
 
-  const [tabValue, setTabValue] = useState<string | null>(null);
+  const [tabValue, setTabValue] = useState<string>(Tab.HOME);
 
   const {
     register,
@@ -54,74 +54,76 @@ const Home = () => {
         <meta name="description" content="wai history" />
       </Head>
 
-      <main>
-        <div className="container text-center mx-auto my-auto">
-          <div className="row text-5xl mb-5">ワイメンス</div>
-          <ul className="flex">
-            <li className={`${tabValue == Tab.HOME && 'bg-blue-50'}  -mb-px mr-1 `}>
-              <button
-                className="inline-block py-2 px-4 text-blue-800 font-semibold"
-                onClick={() => {
-                  changeTabHandler('Home');
-                }}
-              >
-                Home
-              </button>
-            </li>
-            <li className={`${tabValue == Tab.BIO && 'bg-blue-50'} -mb-px mr-1 `}>
-              <button
-                className=" inline-block py-2 px-4 text-green-500 font-semibold"
-                onClick={() => {
-                  changeTabHandler('Bio');
-                }}
-              >
-                Biography
-              </button>
-            </li>
-            <li className={`${tabValue == Tab.HAB && 'bg-blue-50'} -mb-px mr-1 `}>
-              <button
-                className=" inline-block py-2 px-4 text-yellow-700 font-semibold"
-                onClick={() => {
-                  changeTabHandler('WaiHabits');
-                }}
-              >
-                ワイ習慣
-              </button>
-            </li>
-            <li className={`${tabValue == Tab.LOG && 'bg-blue-50'} -mb-px mr-1 `}>
-              <button
-                className="inline-block rounded-t py-2 px-4 text-blue-700 font-semibold"
-                onClick={() => {
-                  changeTabHandler('WaiLog');
-                }}
-              >
-                ワイ log
-              </button>
-            </li>
-            <li className={`${tabValue == Tab.ABI && 'bg-blue-50'} -mb-px mr-1 `}>
-              <button
-                className="inline-block py-2 px-4 text-blue-700 font-semibold"
-                onClick={() => {
-                  changeTabHandler('WaiAbility');
-                }}
-              >
-                ワイアビリティ
-              </button>
-            </li>
-          </ul>
-          {tabValue == Tab.HOME && <HomeContents />}
-          {tabValue == Tab.BIO && <History />}
-          {/* {tabValue == 'Habits' && <WaiHabits />}
+      <div className="flex flex-col h-screen justify-between">
+        <header className="h-10 text-center text-4xl border-b-2 mb-2">Waimensu</header>
+        <main className="mb-auto h-10 ">
+          <div className="container text-center mx-auto my-auto ">
+            <ul className="flex justify-center">
+              <li className={`${tabValue == Tab.HOME && 'bg-blue-50'}  -mb-px mr-1 `}>
+                <button
+                  className="inline-block py-2 px-4 text-blue-800 font-semibold"
+                  onClick={() => {
+                    changeTabHandler('Home');
+                  }}
+                >
+                  Home
+                </button>
+              </li>
+              <li className={`${tabValue == Tab.BIO && 'bg-blue-50'} -mb-px mr-1 `}>
+                <button
+                  className=" inline-block py-2 px-4 text-green-500 font-semibold"
+                  onClick={() => {
+                    changeTabHandler('Bio');
+                  }}
+                >
+                  Biography
+                </button>
+              </li>
+              <li className={`${tabValue == Tab.HAB && 'bg-blue-50'} -mb-px mr-1 `}>
+                <button
+                  className=" inline-block py-2 px-4 text-yellow-700 font-semibold"
+                  onClick={() => {
+                    changeTabHandler('WaiHabits');
+                  }}
+                >
+                  ワイ習慣
+                </button>
+              </li>
+              <li className={`${tabValue == Tab.LOG && 'bg-blue-50'} -mb-px mr-1 `}>
+                <button
+                  className="inline-block rounded-t py-2 px-4 text-blue-700 font-semibold"
+                  onClick={() => {
+                    changeTabHandler('WaiLog');
+                  }}
+                >
+                  ワイ log
+                </button>
+              </li>
+              <li className={`${tabValue == Tab.ABI && 'bg-blue-50'} -mb-px mr-1 `}>
+                <button
+                  className="inline-block py-2 px-4 text-blue-700 font-semibold"
+                  onClick={() => {
+                    changeTabHandler('WaiAbility');
+                  }}
+                >
+                  ワイアビリティ
+                </button>
+              </li>
+            </ul>
+            {tabValue == Tab.HOME && <HomeContents />}
+            {tabValue == Tab.BIO && <History />}
+            {/* {tabValue == 'Habits' && <WaiHabits />}
           {tabValue == 'log' && <WaiLog />}
           {tabValue == 'ability' && <WaiAbility />} */}
-        </div>
+          </div>
 
-        {/* <form className="" onSubmit={handleSubmit(onSubmit)}>
+          {/* <form className="" onSubmit={handleSubmit(onSubmit)}>
           <input defaultValue="test" {...register('userName')} />
           <input type="submit" />
         </form> */}
-      </main>
-      <footer className="text-center">©️ 2021 waimensu</footer>
+        </main>
+        <footer className="text-center bg-gray-200">©️ 2021 waimensu</footer>
+      </div>
     </>
   );
 };
