532c7182ccb4aca58cd5cab4ac5bb11f9bb3689a
zahmis <meda829ratteru86@gmail.com>
2021-12-18 21:56:42 +0900

    use border

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const Home = () => {
         <main className="mb-auto h-10 ">
           <div className="container text-center mx-auto my-auto ">
             <ul className="flex justify-center">
-              <li className={`${tabValue == Tab.HOME && 'bg-blue-50'}  -mb-px mr-1 `}>
+              <li className={`${tabValue == Tab.HOME && 'border-b-4 border-indigo-700 '}  -mb-px mr-1 `}>
                 <button
                   className="inline-block py-2 px-4 text-blue-800 font-semibold"
                   onClick={() => {
@@ -69,7 +69,7 @@ const Home = () => {
                   Home
                 </button>
               </li>
-              <li className={`${tabValue == Tab.BIO && 'bg-blue-50'} -mb-px mr-1 `}>
+              <li className={`${tabValue == Tab.BIO && 'border-b-4 border-indigo-700'} -mb-px mr-1 `}>
                 <button
                   className=" inline-block py-2 px-4 text-green-500 font-semibold"
                   onClick={() => {
@@ -79,7 +79,7 @@ const Home = () => {
                   Biography
                 </button>
               </li>
-              <li className={`${tabValue == Tab.HAB && 'bg-blue-50'} -mb-px mr-1 `}>
+              <li className={`${tabValue == Tab.HAB && 'border-b-4 border-indigo-700'} -mb-px mr-1 `}>
                 <button
                   className=" inline-block py-2 px-4 text-yellow-700 font-semibold"
                   onClick={() => {
@@ -89,7 +89,7 @@ const Home = () => {
                   ワイ習慣
                 </button>
               </li>
-              <li className={`${tabValue == Tab.LOG && 'bg-blue-50'} -mb-px mr-1 `}>
+              <li className={`${tabValue == Tab.LOG && 'border-b-4 border-indigo-700'} -mb-px mr-1 `}>
                 <button
                   className="inline-block rounded-t py-2 px-4 text-blue-700 font-semibold"
                   onClick={() => {
@@ -99,7 +99,7 @@ const Home = () => {
                   ワイ log
                 </button>
               </li>
-              <li className={`${tabValue == Tab.ABI && 'bg-blue-50'} -mb-px mr-1 `}>
+              <li className={`${tabValue == Tab.ABI && 'border-b-4 border-indigo-700'} -mb-px mr-1 `}>
                 <button
                   className="inline-block py-2 px-4 text-blue-700 font-semibold"
                   onClick={() => {
